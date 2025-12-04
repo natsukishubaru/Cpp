887d20ab318a9b83b20de63cf5e0d011fd4ccb14
Shubham Dahiya <shubhnaam205@gmail.com>
2025-12-04 11:43:11 +0530

    Merge sort

--- DSA/4_Sorting/1_Sort.cpp
+++ DSA/4_Sorting/1_Sort.cpp
@@ -5,6 +5,9 @@ void print_arr(vector<int>);
 vector<int> bubble_sort(vector<int>);
 vector<int> selection_sort(vector<int>);
 vector<int> insertion_sort(vector<int>);
+vector<int> merge_sort(vector<int>);
+vector<int> divide(vector<int>&, int, int);
+vector<int> merge(vector<int>&, int, int, int);
 
 int main(){
     vector<int> arr = {2, 1, 4, 3, 4, 6, 5, 9, 10, 8, 7};
@@ -12,7 +15,7 @@ int main(){
     print_arr(bubble_sort(arr));
     print_arr(selection_sort(arr));
     print_arr(insertion_sort(arr));
-    
+    print_arr(merge_sort(arr));
     return 0;
 }
 
@@ -72,3 +75,43 @@ vector<int> insertion_sort(vector<int> arr){
     return arr;
 }
 
+vector<int> merge_sort(vector<int> arr){
+    return divide(arr, 0, arr.size() - 1);
+}
+vector<int> divide(vector<int>& arr, int low, int high){
+    if (low >= high) return arr;
+    divide(arr, low, (low+high)/2);
+    divide(arr, (low+high)/2 + 1, high);
+    merge(arr, low, (low+high)/2, high);
+    return arr;
+}
+vector<int> merge(vector<int>& arr, int low, int mid, int high){
+    int p1 = low;
+    int p2 = mid + 1;
+    int index = low;
+    vector<int> arr_copy = arr;
+
+    while ((p1 <= mid) && (p2 <= high)){
+        if (arr_copy[p1] < arr_copy[p2]){
+            arr[index] = arr_copy[p1];
+            index++;
+            p1++;
+        }
+        else{
+            arr[index] = arr_copy[p2];
+            index++;
+            p2++;
+        }
+    }
+    while (p1 <= mid){
+        arr[index] = arr_copy[p1];
+        index++;
+        p1++;
+    }
+    while (p2 <= high){
+        arr[index] = arr_copy[p2];
+        index++;
+        p2++;
+    }
+    return arr;
+}
